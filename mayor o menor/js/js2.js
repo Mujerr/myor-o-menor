831340b4f15d7752bff64be6a3524ec831b063f7
Martalo <91905695+Mujerr@users.noreply.github.com>
2022-02-14 10:29:36 +0100

    Add files via upload

---
+++ mayor o menor/js/js2.js
@@ -0,0 +1,11 @@
+
+function edadmayor(){
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+if (edad >= 18){
+    alert(nombre + " eres mayor de edad");
+}
+else {
+    alert (nombre + " eres menor de edad");  
+}
+}
